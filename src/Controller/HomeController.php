0315aa7ebf4da187bda6380f4e69ba60d843d77d
damienlebon <damienlebon.ifpa@gmail.com>
2024-06-20 00:18:32 +0200

    add: three latest articles and paints

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogPostRepository;
+use App\Repository\PaintRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PaintRepository $paintRepository, BlogPostRepository $blogPostRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'last_three_paints' => $paintRepository->getLastThree(),
+            'last_three_blog_post' => $blogPostRepository->getLastThree(),
         ]);
     }
 }
