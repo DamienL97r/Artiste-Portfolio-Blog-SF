75c277642599f2a5668018862219bbc4288c5eaf
damienlebon <damienlebon.ifpa@gmail.com>
2024-06-21 02:45:03 +0200

    add: mailer feature

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Services\MailerService;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function __construct(private MailerService $mailerService) {}
+
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function contact(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $contact->setCreatedAt(new DateTime('now'));
+            $contact->setSend(false);
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $this->mailerService->sendMail(
+                $contact->getEmail(),
+                'peintre@lyon.com',
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                ['contact' => $contact]
+            );
+
+            $this->addFlash(
+                'success',
+                'Votre message a été envoyé avec succès !'
+            );
+
+            // return $this->redirectToRoute('app_contact', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Services/MailerService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+
+readonly class MailerService
+{
+    public function __construct(private MailerInterface $mailer) {}
+
+    public function sendMail(
+        string $sender,
+        string $recipient,
+        string $subject,
+        string $htmlTemplate,
+        array $context
+    ): void {
+        $email = (new TemplatedEmail())
+                ->from($sender)
+                ->to($recipient)
+                ->subject($subject)
+                ->htmlTemplate($htmlTemplate)
+                ->context($context);
+
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+        }
+    }
+}
