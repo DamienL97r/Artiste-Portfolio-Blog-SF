7b5de7224586329be18caea6dd7990615c647079
damienlebon <damienlebon.ifpa@gmail.com>
2024-06-21 18:09:27 +0200

    add event: deletion of img when deleting assciated article

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -5,25 +5,30 @@ namespace App\EventSubscriber;
 use App\Entity\BlogPost;
 use DateTime;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityDeletedEvent;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
     private $slugger;
     private $security;
+    private $filesystem;
 
-    public function __construct(SluggerInterface $slugger, Security $security)
+    public function __construct(SluggerInterface $slugger, Security $security, Filesystem $filesystem)
     {
         $this->slugger = $slugger;
         $this->security = $security;
+        $this->filesystem = $filesystem;
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
             BeforeEntityPersistedEvent::class => ['setBlogPostSlugAndDateAndUser'],
+            BeforeEntityDeletedEvent::class => ['removeBlogPostImage']
         ];
     }
 
@@ -44,4 +49,22 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         $user = $this->security->getUser();
         $entity->setUser($user);
     }
+
+    public function removeBlogPostImage(BeforeEntityDeletedEvent $event)
+{
+    $entity = $event->getEntityInstance();
+
+    if (!($entity instanceof BlogPost)) {
+        return;
+    }
+
+    $imageFilename = $entity->getFile();
+    if ($imageFilename) {
+        $imagePath = __DIR__ . '/../../public/uploads/images/' . $imageFilename;
+
+        if (file_exists($imagePath)) {
+            $this->filesystem->remove($imagePath);
+        }
+    }
+}
 }
